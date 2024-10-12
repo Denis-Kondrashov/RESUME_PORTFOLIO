ca2a637614c635a85352260a777544e53b93cb11
DenisK5 <denis.kondrashov1@gmail.com>
2024-10-12 12:30:00 +0300

    feat: написаны модели, подключены media

--- resume/portfolio/models.py
+++ resume/portfolio/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    link = models.URLField(blank=True)
+    image = models.ImageField(upload_to='upload_media/')
+
+    def __str__(self):
+        return self.title
+
+
+class Education(models.Model):
+    field_of_study = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.institution
+
+
+class WorkExperience(models.Model):
+    company = models.CharField(max_length=150)
+    position = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.position} at {self.company}"
