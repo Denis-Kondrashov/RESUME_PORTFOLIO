c8f324dc477c0c78f1cc7520f0f2b15f5086be9e
DenisK5 <denis.kondrashov1@gmail.com>
2024-10-12 14:54:08 +0300

    feat: модели дописаны, произведена локализация, модели добавлены в админку

--- resume/portfolio/admin.py
+++ resume/portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Skill, Project, Education, WorkExperience
+
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(Education)
+admin.site.register(WorkExperience)
--- resume/portfolio/models.py
+++ resume/portfolio/models.py
@@ -2,36 +2,67 @@ from django.db import models
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Навык"
+        verbose_name_plural = "Навыки"
 
     def __str__(self):
         return self.name
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.TextField()
-    link = models.URLField(blank=True)
-    image = models.ImageField(upload_to='upload_media/')
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    link = models.URLField(blank=True, verbose_name="Ссылка")
+    image = models.ImageField(
+        upload_to='upload_media/',
+        verbose_name="Изображение"
+        )
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
 
     def __str__(self):
         return self.title
 
 
 class Education(models.Model):
-    field_of_study = models.CharField(max_length=100)
+    field_of_study = models.CharField(
+        max_length=100,
+        verbose_name="Специальность"
+        )
+    institution = models.CharField(
+        max_length=100,
+        verbose_name="Учебное заведение",
+        default="Не указано"
+        )
+
+    class Meta:
+        verbose_name = "Образование"
+        verbose_name_plural = "Образование"
 
     def __str__(self):
         return self.institution
 
 
 class WorkExperience(models.Model):
-    company = models.CharField(max_length=150)
-    position = models.CharField(max_length=100)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField(blank=True, null=True)
+    company = models.CharField(max_length=150, verbose_name="Компания")
+    position = models.CharField(max_length=100, verbose_name="Должность")
+    description = models.TextField(verbose_name="Описание")
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата окончания"
+        )
+
+    class Meta:
+        verbose_name = "Опыт работы"
+        verbose_name_plural = "Опыт работы"
 
     def __str__(self):
-        return f"{self.position} at {self.company}"
+        return f"{self.position} в {self.company}"
