0374f5e7374f8e07e06ed6221b68c43753a7f015
DenisK5 <denis.kondrashov1@gmail.com>
2024-10-13 13:00:23 +0300

    feat: улучшена админка

--- resume/portfolio/urls.py
+++ resume/portfolio/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +10,4 @@ urlpatterns = [
     path('projects/', views.achievements, name='projects'),
     path('education/', views.education, name='education'),
     path('work-experience/', views.work_experience, name='work_experience'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
