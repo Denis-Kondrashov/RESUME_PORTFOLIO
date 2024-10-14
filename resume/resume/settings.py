39b7c613ea1f86f218488ae16bde642afb64f7da
DenisK5 <denis.kondrashov1@gmail.com>
2024-10-14 12:37:45 +0300

    feat: подключён DjDT

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'portfolio.apps.PortfolioConfig',
 ]
 
@@ -41,6 +42,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'resume.urls'
