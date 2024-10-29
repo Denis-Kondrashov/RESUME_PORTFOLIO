f447d8c77e3477e1683868d782642b5c3a36f902
root <root@3650307-bn35225.twc1.net>
2024-10-29 15:17:59 +0300

    feat: добавлено доменное имя

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'token')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['147.45.143.194', '127.0.0.1', 'localhost', 'denis-kondrashov.ddns.net']
 
 
 # Application definition
@@ -116,11 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles'
     ]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 
